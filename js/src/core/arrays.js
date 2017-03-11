1c6da317458dbf70936ef4c8d6d4699cd447707b
Audrey Gehring <audreygehring@gmail.com>
2017-03-11 11:35:18 -0500

    initial commit

---
+++ js/src/core/arrays.js
@@ -0,0 +1,49 @@
+let removeWithoutCopy = (arr, item) => {
+  for (var i = arr.length; i--;) {
+    if(arr[i] === item) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+};
+
+let append = (arr, item) => {
+  arr.push(item);
+  return arr;
+};
+
+truncate = (arr) => {
+
+};
+
+prepend = (arr, item) => {
+
+};
+
+curtail = (arr) => {
+
+};
+
+concat = (arr1, arr2) => {
+
+};
+
+insert = (arr, item, index) => {
+
+}
+
+count = (arr, item) => {
+
+};
+
+duplicates = (arr) => {
+
+};
+
+square = (arr) => {
+
+};
+
+findAllOccurrences = (arr, item) => {
+
+};
